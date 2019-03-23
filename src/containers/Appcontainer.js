fe500414abebc971d05efa3941a784ed045d8367
Takanori Fukuyama <takanori.fukuyama@ngl.technology>
2019-03-23 09:16:34 +0900

    add container

---
+++ src/containers/Appcontainer.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import { connect } from 'react-redux'
+import App from '../conponents/App'
+
+const mapStateToProps = (state) => {
+    console.log(state.auth.uid)
+    return {
+        uid: state.auth.uid,
+    }
+}
+const AppContainer = connect(
+    mapStateToProps
+)(App)
+
+export default AppContainer;
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyDzIsv_AMuO1sY9O2r3b4sOnPYA1h4bGgg",
+    authDomain: "freeworks-9bb68.firebaseapp.com",
+    databaseURL: "https://freeworks-9bb68.firebaseio.com",
+    projectId: "freeworks-9bb68",
+    storageBucket: "freeworks-9bb68.appspot.com",
+    messagingSenderId: "636066987238"
+};
