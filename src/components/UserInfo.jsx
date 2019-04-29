08e2197693f7ef4eaf0a9c7f98ca48025691b731
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2019-04-30 04:17:55 +0900

    add map component

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -6,15 +6,15 @@ import Grid from "@material-ui/core/Grid"
 
 export default class UserInfo extends React.Component {
     
-    render () {
-        return (
-            <div>
-                <Grid container justify="center" alignItems="center" >
-                    <Avatar style={{margin:10}} src={this.props.photoURL} />
-                </Grid>
-                <p>Hello!  { this.props.displayName }</p>
-                <Button variant="contained" color="primary" onClick={this.props.doLogout}>Logout</Button>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <Grid container justify="center" alignItems="center" >
+          <Avatar style={{margin:10}} src={this.props.photoURL} />
+        </Grid>
+        <p>Hello!  { this.props.displayName }</p>
+        <Button variant="contained" color="primary" onClick={this.props.doLogout}>Logout</Button>
+      </div>
+    )
+  }
 }
